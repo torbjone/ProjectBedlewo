228341e672ad18933d61be7e7b52ac872677e232
Torbjørn B Ness <torbness@gmail.com>
2012-08-22 00:01:54 +0200

    Nice all-around plots of stuff

--- plot_mea.py
+++ plot_mea.py
@@ -7,11 +7,12 @@ def plot_alot(signal_at_elec, cell, t_range, Mea, out_name):
     electrode_separation = Mea.electrode_separation
     t_array = cell.tvec
     fig = pl.figure(figsize=[10, 10])
-    ax = fig.add_axes([0.3, 0.05, 0.7, .9], frameon=False)
+    ax = fig.add_axes([0.25, 0.05, 0.7, .9], frameon=False)
     ax.axis([-0.3,0.6,-0.2,1.1])
     #ax.axis('equal')
     n_compartments = len(cell.diam)#neuron.total_n_compartments
-
+    stim_points = [615, 671,623, 628] 
+    comp_list = np.r_[0, 615, 646, 671, 623, 628, 657]
     for i in xrange(n_compartments):
         if i == 0:
             xcoords = pl.array([cell.xmid[i]/1000])
@@ -21,14 +22,23 @@ def plot_alot(signal_at_elec, cell, t_range, Mea, out_name):
         else:
             if cell.zmid[i] < 100 and cell.zmid[i] > -100 and \
                     cell.xmid[i] < 100 and cell.xmid[i] > -100:
-                xcoords = pl.r_[xcoords, pl.linspace(cell.xstart[i], cell.xend[i], cell.length[i]*3)/1000]
-                ycoords = pl.r_[ycoords, pl.linspace(cell.ystart[i], cell.yend[i], cell.length[i]*3)/1000]   
-                zcoords = pl.r_[zcoords, pl.linspace(cell.zstart[i], cell.zend[i], cell.length[i]*3)/1000]   
-                diams = pl.r_[diams, pl.linspace(cell.diam[i], cell.diam[i],cell.length[i]*3)]
+                xcoords = pl.r_[xcoords, pl.linspace(cell.xstart[i], \
+                            cell.xend[i], cell.length[i]*3)/1000]
+                ycoords = pl.r_[ycoords, pl.linspace(cell.ystart[i], \
+                            cell.yend[i], cell.length[i]*3)/1000]   
+                zcoords = pl.r_[zcoords, pl.linspace(cell.zstart[i], \
+                            cell.zend[i], cell.length[i]*3)/1000]   
+                diams = pl.r_[diams, pl.linspace(cell.diam[i], \
+                            cell.diam[i],cell.length[i]*3)]
     argsort = pl.argsort(-xcoords)
-    ax.scatter(zcoords[argsort], ycoords[argsort], s=2*diams[argsort]**2,
-                   edgecolors='none', c='gray')#, cmap='gray',c=xcoords[argsort])    
-    ax.scatter(Mea.elec_z, Mea.elec_y, color='m', s= 2)
+    ax.scatter(zcoords[argsort], ycoords[argsort], s=3*diams[argsort]**2,\
+               edgecolors='none', c='gray')
+    ax.scatter(cell.zmid[0]/1000, cell.ymid[0]/1000, \
+               s=3*cell.diam[0]**2, edgecolors='none', c='gray')    
+    ax.scatter(Mea.elec_z, Mea.elec_y, color='m', s= 50)
+    for comp in stim_points:
+        ax.plot(cell.zmid[comp]/1000, cell.ymid[comp]/1000, '*', color='y')
+
 
     t_start = t_range[0]
     t_stop = t_range[1]
@@ -37,10 +47,11 @@ def plot_alot(signal_at_elec, cell, t_range, Mea, out_name):
     t_array = t_array[t_start_index:t_stop_index] - t_array[t_start_index]
     signal_at_elec = signal_at_elec[:,t_start_index:t_stop_index]
     
-    #chosen_imem = chosen_imem[t_start_index:t_stop_index]    
-    time_factor = 1./(t_stop - t_start)*electrode_separation/2
-    signal_range = 15#np.max(signal_at_elec) - np.min(signal_at_elec)
-    pot_factor  = electrode_separation/signal_range
+    #chosen_imem = chosen_imem[t_start_index:t_stop_index]
+    time_factor = 0.7*1./(t_stop - t_start)*electrode_separation/2
+    signal_range = 15
+    pot_factor  = 0.5*electrode_separation/signal_range
+    
     for elec in xrange(n_elecs):
         t = t_array*time_factor + Mea.elec_z[elec]
         trace = (signal_at_elec[elec])*pot_factor + Mea.elec_y[elec]  
@@ -53,21 +64,34 @@ def plot_alot(signal_at_elec, cell, t_range, Mea, out_name):
     #ax.axis([-0.1, 0.1, -0.1, 0.1])           
     #ax.axis([1.1*np.min(Mea.elec_z),1.1*np.max(Mea.elec_z) , 1.1*np.min(Mea.elec_y),1.1*np.max(Mea.elec_y) ])
     
-    ax.plot([0.3, 0.3 + electrode_separation/2], [0.1, 0.1], color='k', lw = 4)
-    ax.text(0.32, 0.105, '%g ms' % int(t_stop-t_start))
+    ax.plot([0.2, 0.2 + time_factor *(t_stop - t_start)], \
+            [-0.13, -0.13], color='k', lw = 4)
+    ax.text(0.205, -0.155, '%g ms' % int(t_stop-t_start))
     
-    ax.plot([0.305, 0.355], [0.01, 0.01], color='k', lw = 4)
-    ax.text(0.32, 0.06, '50 $\mu$m')
+    #ax.plot([0.305, 0.355], [0.01, 0.01], color='k', lw = 4)
+    #ax.text(0.32, 0.06, '50 $\mu$m')
     
-    ax.plot([0.3, 0.3], [0.01, 0.01 + electrode_separation], color='r', lw=4)
-    ax.text(0.32, 0.01 +electrode_separation/2 , '%g $\mu$V' % signal_range)
+    ax.plot([0.2, 0.2], [-0.13, -0.13 +pot_factor * signal_range],\
+            color='r', lw=4)
+    ax.text(0.205, -.21 +electrode_separation/2 , '%g $\mu$V' % signal_range)
 
     #pl.xlabel('z [$\mu$m]')
     #pl.ylabel('y [$\mu$m]')
     ax.set_xticks([])
     ax.set_yticks([])
 
-    ax2 = fig.add_axes([0.05, 0.1, 0.2, 0.2])
+    ax2 = fig.add_axes([0.1, 0.1, 0.25, 0.2])
+    ax2.plot([20],[8], '*', color='k')
+    ax2.plot([40],[8], '*', color='k')
+    ax2.plot([60],[8], '*', color='k')
+    #ax2.text(pos[20,5], pos[1,5]-0.15, "Arrow", ha="center",
+    #    family=font, size=14)
+
+    
+    #p = ax2.axvspan(20, 21, facecolor='0.5', alpha=0.2)
+    #p = ax2.axvspan(40, 41, facecolor='0.5', alpha=0.2)
+    #p = ax2.axvspan(60, 61, facecolor='0.5', alpha=0.2)    
+    ax2.axis([10,100, -100,20])
     ax2.set_title('Membrane voltage')
     ax2.set_xlabel('Time [ms]')
     ax2.set_ylabel('mV')
@@ -80,24 +104,33 @@ def plot_alot(signal_at_elec, cell, t_range, Mea, out_name):
         ax2.plot(cell.tvec[t_start_index:t_stop_index], \
              cell.vmem[comp,t_start_index:t_stop_index])
         
-    ax3 = fig.add_axes([0.05, 0.4, 0.2, 0.2])
+    ax3 = fig.add_axes([0.1, 0.4, 0.25, 0.2])
     ax3.set_title('Membrane currents')
     ax3.set_xlabel('Time [ms]')
+    ax3.axis([10,100, -1,1])
+    ax3.plot([20],[.8], '*', color='k')
+    ax3.plot([40],[.8], '*', color='k')
+    ax3.plot([60],[.8], '*', color='k')
+    
     ax3.set_ylabel('nA')
     #ax2.plot(cell.tvec[t_start_index:t_stop_index], \
     #         cell.vmem[0,t_start_index:t_stop_index])
     #import random
     #comp = random.choice(cell.get_idx_section('apic[63]'))
-    comp_list = np.r_[0, 615, 646, 671, 623, 628, 657]
-    for comp in comp_list:
+    
+    for comp in xrange(len(cell.imem[:,0])):#comp_list:
         ax3.plot(cell.tvec[t_start_index:t_stop_index], \
              cell.imem[comp,t_start_index:t_stop_index])
 
-    ax4 = fig.add_axes([0.05, 0.7, 0.2, 0.2])
+    ax4 = fig.add_axes([0.1, 0.7, 0.25, 0.2])
+    ax4.plot([20],[8], '*', color='k')
+    ax4.plot([40],[8], '*', color='k')
+    ax4.plot([60],[8], '*', color='k')
+
     ax4.set_title('Signal at electrodes')
     ax4.set_xlabel('Time [ms]')
     ax4.set_ylabel('$\mu$V')
-
+    ax4.axis([10,100, -15,10])
     for elec in xrange(Mea.n_elecs):
         ax4.plot(cell.tvec[t_start_index:t_stop_index], \
              signal_at_elec[elec,:])
